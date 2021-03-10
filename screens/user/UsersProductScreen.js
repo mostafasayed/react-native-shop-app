bdd7e78ec3f741e144a931069fa78737d389df0d
Mostafa Hanafy <Mostafa.Hanafy@alrowads.com>
2021-03-10 10:48:22 +0400

    Handle error that may occurs while pulling product from firebase

--- screens/user/UsersProductScreen.js
+++ screens/user/UsersProductScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FlatList, Button, Alert } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
@@ -8,6 +8,7 @@ import ProductItem from "../../components/shop/ProductItem";
 import Colors from "../../constants/Colors";
 
 const UsersProductScreen = (props) => {
+  const [error, setError] = useState();
   const userProducts = useSelector((state) => state.products.userProducts);
   const dispatch = useDispatch();
 
@@ -17,12 +18,19 @@ const UsersProductScreen = (props) => {
       {
         text: "Yes",
         style: "destructive",
-        onPress: () => {
-          dispatch(deleteProduct(id));
+        onPress: async () => {
+          try {
+            await dispatch(deleteProduct(id));
+          } catch (err) {
+            setError(err.message);
+          }
         },
       },
     ]);
   };
+  useEffect(() => {
+    if (error) Alert.alert("Error", error, [{ text: "Ok" }]);
+  }, [error]);
   return (
     <FlatList
       data={userProducts}
